80f50117045c1cc16a2b5c6bcfeaab1627e7ecde
iliaakaloup <iliaskalou@gmail.com>
2023-09-28 11:26:20 +0300

    .

--- Program.cs
+++ Program.cs
@@ -5,9 +5,5 @@ class Program
     static void Main()
     {
         Console.WriteLine("Hello, World!");
-
-        // Create an instance of SecondFile and call its method
-        SecondFile secondFile = new SecondFile();
-        secondFile.PrintMessage();
     }
 }
